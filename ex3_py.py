3261c4ee600419e097c5da5f4d56c01a312fb089
YanaPatyuk <yanch22@gmail.com>
2019-06-08 11:01:13 +0300

    Update

--- ex3_py.py
+++ ex3_py.py
@@ -1,5 +1,4 @@
 import numpy as np
-import scipy as sp
 
 
 def softmax(X):
@@ -22,14 +21,65 @@ def softmax_derivative(X):
   #  return dx
                  """""
 
-def training(etha, train_x, train_y,ep_num,params):
+def training(etha, train_x, train_y,ep_num,params,dev_x,dev_y):
+    rnd = np.arange(train_x.shape[0])
     for i in range(ep_num):
         sum = 0.0
+        #shuffle
+        np.random.shuffle(rnd)
+        train_x = train_x[rnd]
+        train_y = train_y[rnd]
         for cur_x,cur_y in zip(train_x,train_y):
-            forward_ret = forward_prop(cur_y,cur_x,params)
+            forward_ret = forward_prop(cur_x,cur_y,params)
+            back_ret = back_prop(forward_ret)
             sum+= forward_ret['loss']
+            params = update_param(forward_ret, back_ret, etha)
         loss_avg = sum / train_x.shape[0]
         print(loss_avg)
+        predict_on_dev(params,dev_x,dev_y)
+
+    return params
+
+
+def predict_on_dev(params, dev_x, dev_y):
+ sum_loss = good = 0.0 # good counts how many times we were correct
+ for x, y in zip(dev_x, dev_y):
+    out = forward_prop(x, y, params) # get probabilities vector as result, where index y is the probability that x is classifiedas tag y
+    loss = out['loss']
+    sum_loss += loss
+    if out['h2'].argmax() == y: # model was correct
+        good += 1
+ acc = good / dev_x.shape[0] # how many times we were correct / # of examples
+ avg_loss = sum_loss / dev_x.shape[0] # avg. loss
+ print(avg_loss, acc)
+ #return avg_loss, acc
+
+
+def testing(test_x, params):
+    f = open("test_y", "w")
+    for x in test_x:
+        out = forward(params, x)
+        f.write(str(out.argmax()))
+    f.close()
+
+def forward(params,x):
+    W1, b1, W2, b2 = [params[key] for key in ('W1', 'b1', 'W2', 'b2')]
+    z1 = np.dot(W1, x.reshape(784, 1)) + b1
+    h1 = relu_activation(z1)
+    z2 = np.dot(W2, h1) + b2
+    h2 = softmax(z2)
+    return h2
+
+def update_param(old_params, grad_params, eta):
+    W1, b1, W2, b2 = [old_params[key] for key in ('W1', 'b1', 'W2', 'b2')]
+    W1_new, b1_new, W2_new, b2_new = [grad_params[key] for key in ('W1', 'b1', 'W2', 'b2')]
+
+    W1 = W1 - eta * W1_new
+    b1 = b1 - eta * b1_new
+    W2 = W2 - eta * W2_new
+    b2 = b2 - eta * b2_new
+    return {'b1': b1, 'W1': W1, 'b2': b2, 'W2': W2}
+
 
 def relu_activation(X):
     return np.maximum(X, 0)
@@ -39,6 +89,7 @@ def relu_deriative(X):
 
 def forward_prop(x, y, params):
     W1, b1, W2, b2 = [params[key] for key in ('W1', 'b1', 'W2', 'b2')]
+
     z1 = np.dot(W1, x.reshape(784, 1)) + b1
     h1 = relu_activation(z1)
     z2 = np.dot(W2, h1) + b2
@@ -52,11 +103,14 @@ def forward_prop(x, y, params):
 def back_prop(fprop_cache):
   # Follows procedure given in notes
   x, y, z1, h1, z2, h2, loss = [fprop_cache[key] for key in ('x', 'y', 'z1', 'h1', 'z2', 'h2', 'loss')]
-  dz2 = (h2 - y)                                #  dL/dz2
+  vec_y = np.array([0]*10)
+  vec_y[int(y)] = 1
+  dz2 = (h2 - vec_y)                                #  dL/dz2
+
   dW2 = np.dot(dz2, h1.T)                       #  dL/dz2 * dz2/dw2
 
   db2 = dz2                                     #  dL/dz2 * dz2/db2 =  dL/dz2
-  dz1 = np.dot(fprop_cache['W2'].T, (h2 - y)) * relu_deriative(z1) #  dL/dz2 * dz2/dh1 * dh1/dz1
+  dz1 = np.dot(fprop_cache['W2'].T, (h2 - vec_y)) * relu_deriative(z1) #  dL/dz2 * dz2/dh1 * dh1/dz1
 
   dW1 = np.dot(dz1, x.T)                        #  dL/dz2 * dz2/dh1 * dh1/dz1 * dz1/dw1
   db1 = dz1                                     #  dL/dz2 * dz2/dh1 * dh1/dz1 * dz1/db1
@@ -79,16 +133,26 @@ def convart_classificaion_one_hot(classifications):
 
 if __name__ == "__main__":
     # Initialize random parameters and inputs
-    W1 = np.random.rand(2, 2)
-    b1 = np.random.rand(2, 1)
-    W2 = np.random.rand(1, 2)
-    b2 = np.random.rand(1, 1)
+    input_size = 784
+    h_rows_size = 50
+    num_of_classes = 10
+    # Initialize random parameters and inputs
+    W1 = np.random.uniform(-0.08, 0.08, [h_rows_size, input_size])
+    b1 = np.random.rand(h_rows_size, 1)
+    W2 = np.random.uniform(-0.08, 0.08, [num_of_classes, h_rows_size])
+    b2 = np.random.rand(num_of_classes, 1)
     params = {'W1': W1, 'b1': b1, 'W2': W2, 'b2': b2}
     epoch = 10
     #load the text file with the data
     x_train = np.loadtxt("train_x", max_rows=255)/255.0
     y_train = np.loadtxt("train_y", max_rows=255)
     test_x = np.loadtxt("test_x")
-    training(0.01, x_train, y_train, 10, params)
+
+    dev_size = (int)(x_train.shape[0]*0.2)
+    dev_x,dev_y = x_train[-dev_size:, :], y_train[-dev_size:]
+    x_train, y_train = x_train[:dev_size,:], y_train[:-dev_size]
+
+    params = training(0.01, x_train, y_train, 10, params, dev_x, dev_y)
+    testing(test_x,params)
 
 
